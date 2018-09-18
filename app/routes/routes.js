eec5a0f17bf8af6e89715035cb2006a212257af9
Michael <mkeating2225@gmail.com>
2018-09-18 17:21:30 -0400

    adding basic twitter test

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,24 @@
 //routes/routes.js
+const twit = require('twit')
+const config = = {
+  consumer_key: 		process.env.twitConsumerKey,  
+  consumer_secret: 		process.env.twitConsumerSecret,
+  access_token: 		process.env.twitAccessToken,  
+  access_token_secret: 	process.env.twitAccessTokenSecret,
+  timeout_ms:           5*1000
+}
 
+const Twitter = new twit(config);
 
 module.exports = function(app){
 	app.post('/tweets', (req, res) => {
 
-		const responseText = `You have sent me ${req.body.body}`
-		res.send(process.env.twitKey)
+		const searchQuery = req.body.body
+		
+		Twitter.get('search/tweets', {q: searchQuery, count: 1}, (err, data, response) =>{
+			res.send(data);
+		})
+
+
 	})
 }
