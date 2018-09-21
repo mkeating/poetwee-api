145d4fa7020820c6dcdfd5def60ccaecd43a8a88
Michael <mkeating2225@gmail.com>
2018-09-21 18:03:57 -0400

    deploy to heroku

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -19,11 +19,11 @@ module.exports = function(app){
 		const searchQuery = req.body.body
 		
 		//split the words and add spaces for better searching (only search for distinct words)
-		const searchWords = searchQuery.split(' ').map(word => ` ${word} `);
+		//const searchWords = searchQuery.split(' ').map(word => ` ${word} `);
 		
 
 		Twitter.get('search/tweets', {q: searchQuery, count: 10}, (err, data, response) =>{
-			res.json(data.statuses);
+			res.json(data);
 
 		})
 
