6259c7a8fae6c259697ecb9c2363c466fcc01ece
Michael <mkeating2225@gmail.com>
2018-09-21 17:14:15 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -22,7 +22,37 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 
-require('./app/routes')(app)
+const twit = require('twit')
+const config = {
+  consumer_key: 		process.env.twitConsumerKey,  
+  consumer_secret: 		process.env.twitConsumerSecret,
+  access_token: 		process.env.twitAccessToken,  
+  access_token_secret: 	process.env.twitAccessTokenSecret,
+  timeout_ms:           5*1000
+}
+
+
+
+const Twitter = new twit(config);
+
+	app.post('/tweets', (req, res) => {
+
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		const searchQuery = req.body.body
+		
+		//split the words and add spaces for better searching (only search for distinct words)
+		const searchWords = searchQuery.split(' ').map(word => ` ${word} `);
+		
+
+		Twitter.get('search/tweets', {q: searchQuery, count: 1}, (err, data, response) =>{
+			res.json(data);
+		})
+
+
+	})
+
+
+//require('./app/routes')(app)
 
 app.listen(port, () => {
 	console.log(`we are live on ${port}`)
